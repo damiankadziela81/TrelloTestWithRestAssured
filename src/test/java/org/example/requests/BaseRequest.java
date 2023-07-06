a4a2fca1e54a0296d38a1fb2210233f779bd51b1
damiankadziela81 <damiankadziela02@gmail.com>
2023-07-06 10:00:24 +0200

    Add logging filters for request and response

--- src/test/java/org/example/requests/BaseRequest.java
+++ src/test/java/org/example/requests/BaseRequest.java
@@ -1,6 +1,8 @@
 package org.example.requests;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.example.secrets.TrelloSecrets;
@@ -14,6 +16,8 @@ public class BaseRequest {
         requestBuilder.setContentType(ContentType.JSON);
         requestBuilder.addQueryParam("key", TrelloSecrets.getKey());
         requestBuilder.addQueryParam("token", TrelloSecrets.getToken());
+        requestBuilder.addFilter(new RequestLoggingFilter());
+        requestBuilder.addFilter(new ResponseLoggingFilter());
 
         return requestBuilder.build();
     }

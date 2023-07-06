089417938e7c99e110b1e7d26cf3f840de12115a
damiankadziela81 <damiankadziela02@gmail.com>
2023-07-06 10:01:13 +0200

    Add test parametrization

--- src/test/java/org/example/tests/boards/CreateBoardTest.java
+++ src/test/java/org/example/tests/boards/CreateBoardTest.java
@@ -5,19 +5,23 @@ import io.restassured.response.Response;
 import org.assertj.core.api.Assertions;
 import org.example.requests.board.CreateBoardRequest;
 import org.example.requests.board.DeleteBoardRequest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 class CreateBoardTest {
 
-
-    private final String boardName = "Pierwszy board";
     private String boardId;
 
-    @Test
-    void createBoardTest() {
+    @DisplayName("Create a board with valid data")
+    @ParameterizedTest(name = "Board name: {0}")
+    @MethodSource("sampleBoardNameData")
+    void createBoardTest(String boardName) {
 
         Map<String, String> queryParams = new HashMap<>();
         queryParams.put("name", boardName);
@@ -34,5 +38,21 @@ class CreateBoardTest {
         // DELETE BOARD
         final Response responseAfterDelete = DeleteBoardRequest.deleteBoardRequest(boardId);
         Assertions.assertThat(responseAfterDelete.statusCode()).isEqualTo(200);
+    }
+
+    private static Stream<Arguments> sampleBoardNameData() {
+        return Stream.of(
+                Arguments.of("nazwaTablicy"),
+                Arguments.of("NAZWA TABLICY"),
+                Arguments.of("NAZWA_TABLICY"),
+                Arguments.of("!"),
+                Arguments.of("@"),
+                Arguments.of("#"),
+                Arguments.of("$"),
+                Arguments.of("%"),
+                Arguments.of("^"),
+                Arguments.of("&"),
+                Arguments.of("*")
+        );
     }
 }
